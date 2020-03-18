0fd849390270cc61efc2b71eebf0d6c7be178e66
Harvey <dez.osk@gmail.com>
2020-03-18 23:23:11 +0000

    Adding initial components, preparing scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <div id="app">
+    <div class="container">
+      <pp-header></pp-header>
+    </div>
+    <div class="container">
+      <pp-home></pp-home>
+    </div>
+    <div class="container">
+      <pp-footer></pp-footer>
+    </div>
   </div>
 </template>
 
-<style>
-</style>
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Home from "@/components/Home";
+
+export default {
+  components: {
+    ppHeader: Header,
+    ppFooter: Footer,
+    ppHome: Home
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="row">
+    <nav class="navbar navbar-expand-md navbar-light">
+      <a class="navbar-brand" href="/">PIZZA PLACE</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Menu</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+header {
+  margin-bottom: 20px;
+}
+.navbar-brand {
+  font-size: 1.5em;
+}
+</style>
