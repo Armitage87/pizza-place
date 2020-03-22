0a1800875000bf162d859d838212522b0ab352ab
Harvey <dez.osk@gmail.com>
2020-03-22 00:17:36 +0000

    Adding menu and admin page

---
+++ src/components/Admin.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+            
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <h3>
+                Menu:
+            </h3>
+            <table class="table table-hover">
+                <thead class="thead-default">
+                    <tr>
+                        <th>Item</th>
+                        <th>Remove From Menu</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Margharitta</td>
+                        <td><button class="btn btn-danger btn-sm">x</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
